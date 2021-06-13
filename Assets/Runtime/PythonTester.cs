442d89c51494bb1e5b929d91f938639e8605a10a
Nixon <voxell.technologies@gmail.com>
2021-06-13 19:48:16 +0800

    created test script where python uses Debug.Log to print a message to unity + remove ironpython nuget package and added official release from ironpython github

---
+++ Assets/Runtime/PythonTester.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
+using SmartAssistant.Core.Inspector;
+
+public class PythonTester : MonoBehaviour
+{
+  // Start is called before the first frame update
+  [Button]
+  public void TestPython()
+  {
+    ScriptEngine pythonEngine = Python.CreateEngine();
+    pythonEngine.Runtime.LoadAssembly(typeof(GameObject).Assembly);
+    ICollection<string> searchPaths = pythonEngine.GetSearchPaths();
+
+    //Path to the Python standard library
+    string[] libPaths = Directory.GetDirectories($"{Application.dataPath}/IronPython/Lib/");
+    searchPaths.Add($"{Application.dataPath}/IronPython/Lib/");
+    foreach (string path in libPaths) searchPaths.Add(path);
+
+    //Path to the folder of main.py
+    string[] mainPaths = Directory.GetDirectories($"{Application.dataPath}/Runtime/PythonFiles/");
+    searchPaths.Add($"{Application.dataPath}/Runtime/PythonFiles/");
+    foreach (string path in mainPaths) searchPaths.Add(path);
+    pythonEngine.SetSearchPaths(searchPaths);
+
+    ScriptScope scriptScope = pythonEngine.ExecuteFile($"{Application.dataPath}/Runtime/PythonFiles/main.py");
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+  }
+}
